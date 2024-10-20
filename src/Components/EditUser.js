44895072cb13b99b10307b46267f492b4a22a844
Huynh Kien Hao <haohk.hkits@gmail.com>
2024-10-20 20:28:35 +0700

    video 84

--- src/Components/EditUser.js
+++ src/Components/EditUser.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 
 class EditUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: this.props.userEditObjectChild.id,
+            name: this.props.userEditObjectChild.name,
+            tel: this.props.userEditObjectChild.tel,
+            permission: this.props.userEditObjectChild.permission
+        }
+    }
+
+    isChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            [name]: value
+        });
+    }
     render() {
         return (
             <div className='row'>
@@ -16,6 +33,7 @@ class EditUser extends Component {
                                         className="form-control"
                                         placeholder="Tên user"
                                         defaultValue={this.props.userEditObjectChild.name}
+                                        onChange={(event) => this.isChange(event)}
                                     />
                                 </div>
                                 <div className="form-group mb-2">
@@ -25,10 +43,16 @@ class EditUser extends Component {
                                         className="form-control"
                                         placeholder="Điện thoại"
                                         defaultValue={this.props.userEditObjectChild.tel}
+                                        onChange={(event) => this.isChange(event)}
                                     />
                                 </div>
                                 <div className="form-group mb-3">
-                                    <select className="form-select" name="permission" defaultValue={this.props.userEditObjectChild.permission} required>
+                                    <select
+                                        className="form-select"
+                                        name="permission"
+                                        defaultValue={this.props.userEditObjectChild.permission}
+                                        onChange={(event) => this.isChange(event)}
+                                        required>
                                         <option>Chọn quyền mặc định</option>
                                         <option value={1}>Admin</option>
                                         <option value={2}>Moderator</option>
